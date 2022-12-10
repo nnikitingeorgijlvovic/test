13b5a400f7d2f4bf2593d5d9029e563de8d92f72
olicoding <oliveira.sein@gmail.com>
2022-12-10 17:03:16 +0100

    Header unit test improved.

--- src/__tests__/header-unit.test.js
+++ src/__tests__/header-unit.test.js
@@ -1,11 +1,23 @@
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "../layout/Header";
 
 test("header-rendering", () => {
-  render(
+  const { getByText } = render(
     <BrowserRouter>
       <Header />
     </BrowserRouter>
   );
+
+  expect(getByText("projects")).toBeInTheDocument();
+  expect(getByText("whoami")).toBeInTheDocument();
+
+  fireEvent.click(getByText("projects"));
+
+  expect(window.location.pathname).toBe("/projects");
+
+  fireEvent.click(getByText("whoami"));
+
+  expect(window.location.pathname).toBe("/whoami");
 });
